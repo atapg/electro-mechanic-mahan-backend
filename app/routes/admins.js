3dd895118f8b44d09cf03c40a31f142c907cdaf3
ata parvin ghods <ataparvinghods@gmail.com>
2021-08-25 21:03:44 +0430

    Image upload route fixed

--- app/routes/admins.js
+++ app/routes/admins.js
@@ -1,218 +1,222 @@
-const routes = require("express").Router()
-const AdminsModel = require("../models/admins")
-const authMiddleware = require("../middlewares/authenticate")
-const adminValidator = require("../validations/admin")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const mongoose = require("mongoose")
+const routes = require("express").Router();
+const AdminsModel = require("../models/admins");
+const authMiddleware = require("../middlewares/authenticate");
+const adminValidator = require("../validations/admin");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+routes.get("/get-admin", authMiddleware, (req, res) => {
+  res.send({ admin: req.authenticatedUser });
+});
 
 routes.get("/authenticate", (req, res) => {
-    if (!req.headers.authorization) {
-        return res.status(401).send({
-            message: "Failed",
-            status: "Unauthorized",
-        })
+  if (!req.headers.authorization) {
+    return res.status(401).send({
+      message: "Failed",
+      status: "Unauthorized",
+    });
+  }
+
+  const authorizationParse = req.headers.authorization.split(" ");
+
+  if (authorizationParse.length < 2) {
+    return res.status(401).send({
+      message: "Failed",
+      status: "Unauthorized",
+    });
+  }
+
+  const token = authorizationParse[1];
+
+  jwt.verify(token, process.env.SECRET, async (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Failed",
+        status: "Unauthorized",
+      });
     }
+  });
 
-    const authorizationParse = req.headers.authorization.split(" ")
-
-    if (authorizationParse.length < 2) {
-        return res.status(401).send({
-            message: "Failed",
-            status: "Unauthorized",
-        })
-    }
-
-    const token = authorizationParse[1]
-
-    jwt.verify(token, process.env.SECRET, async (err, decoded) => {
-        if (err) {
-            return res.status(401).send({
-                message: "Failed",
-                status: "Unauthorized",
-            })
-        }
-    })
-
-    res.status(200).send({
-        message: "Success",
-        status: "Authorized",
-    })
-})
+  res.status(200).send({
+    message: "Success",
+    status: "Authorized",
+  });
+});
 
 routes.post("/login", async (req, res) => {
-    const { email, phone, password } = req.body
+  const { email, phone, password } = req.body;
 
-    let admin
+  let admin;
 
-    if (email) admin = await AdminsModel.findOne({ email })
-    else admin = await AdminsModel.findOne({ phone })
+  if (email) admin = await AdminsModel.findOne({ email });
+  else admin = await AdminsModel.findOne({ phone });
 
-    if (!admin) {
-        return res.status(403).send({
-            status: "Failed",
-            error: "Incorrect credentials!",
-        })
-    }
+  if (!admin) {
+    return res.status(403).send({
+      status: "Failed",
+      error: "Incorrect credentials!",
+    });
+  }
 
-    const correctPass = await bcrypt.compare(password, admin.password)
+  const correctPass = await bcrypt.compare(password, admin.password);
 
-    if (!correctPass) {
-        return res.status(403).send({
-            status: "Failed",
-            error: "Incorrect credentials!",
-        })
-    }
+  if (!correctPass) {
+    return res.status(403).send({
+      status: "Failed",
+      error: "Incorrect credentials!",
+    });
+  }
 
-    const token = jwt.sign({ id: admin._id }, process.env.SECRET, {
-        expiresIn: "7d",
-    })
+  const token = jwt.sign({ id: admin._id }, process.env.SECRET, {
+    expiresIn: "7d",
+  });
 
-    res.send({
-        admin,
-        token,
-    })
-})
+  res.send({
+    admin,
+    token,
+  });
+});
 
 routes.post("/register", authMiddleware, async (req, res) => {
-    const { phone, email, name, password, lastName } = req.body
+  const { phone, email, name, password, lastName } = req.body;
 
-    if (!adminValidator(req.body)) {
-        return res.status(400).send({
-            status: "Failed",
-            error: "Please fill all fields",
-        })
-    }
+  if (!adminValidator(req.body)) {
+    return res.status(400).send({
+      status: "Failed",
+      error: "Please fill all fields",
+    });
+  }
 
-    const existedUser = await AdminsModel.findOne({ phone, email })
+  const existedUser = await AdminsModel.findOne({ phone, email });
 
-    if (existedUser) {
-        return res.status(400).send({
-            status: "Failed",
-            error: "User already exist!",
-        })
-    }
+  if (existedUser) {
+    return res.status(400).send({
+      status: "Failed",
+      error: "User already exist!",
+    });
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    if (hashedPassword) {
-        const admin = await AdminsModel.create({
-            name,
-            lastName,
-            password: hashedPassword,
-            email,
-            phone,
-        })
-
-        if (!admin) {
-            return res.status(400).send({
-                status: "Failed",
-                error: "Something went wrong!",
-            })
-        }
-    }
-
-    res.send({
-        status: "Success",
-    })
-})
-
-routes.delete("/delete", authMiddleware, (req, res) => {
-    const { admin_id } = req.body
-
-    AdminsModel.findByIdAndRemove(admin_id, (err, result) => {
-        if (err) {
-            return res.status(400).send({
-                status: "Failed",
-                error: "Something went wrong!",
-            })
-        }
-    })
-
-    res.send({
-        status: "Success",
-    })
-})
-
-routes.patch("/update", authMiddleware, async (req, res) => {
-    const { name, lastName, phone, email, password, id } = req.body
-
-    const updated = await AdminsModel.findByIdAndUpdate(id, {
-        name,
-        lastName,
-        phone,
-        email,
-    })
-
-    if (!updated) {
-        return res.status(400).send({
-            status: "Failed",
-            error: "Something went wrong!",
-        })
-    }
-
-    if (password) {
-        const hashedPassword = await bcrypt.hash(password, 10)
-
-        AdminsModel.findByIdAndUpdate(
-            id,
-            { password: hashedPassword },
-            (err, result) => {
-                if (err) {
-                    return res.status(400).send({
-                        status: "Failed",
-                        error: "Something went wrong!",
-                    })
-                }
-            }
-        )
-    }
-
-    res.send({
-        status: "Success",
-    })
-})
-
-routes.get("/get-all", authMiddleware, async (req, res) => {
-    const admins = await AdminsModel.find().select("-password")
-
-    if (!admins) {
-        return res.status(400).send({
-            status: "Failed",
-            error: "No admins found!",
-        })
-    }
-
-    res.send({
-        admins,
-    })
-})
-
-routes.get("/:id", authMiddleware, async (req, res) => {
-    const { id } = req.params
-
-    if (!mongoose.isValidObjectId(id)) {
-        return res.status(400).send({
-            status: "Failed",
-            error: "Admin not found!",
-        })
-    }
-
-    const admin = await AdminsModel.findById(id).select("-password")
+  if (hashedPassword) {
+    const admin = await AdminsModel.create({
+      name,
+      lastName,
+      password: hashedPassword,
+      email,
+      phone,
+    });
 
     if (!admin) {
-        return res.status(400).send({
-            status: "Failed",
-            error: "Admin not found!",
-        })
+      return res.status(400).send({
+        status: "Failed",
+        error: "Something went wrong!",
+      });
     }
+  }
 
-    res.send(admin)
-})
+  res.send({
+    status: "Success",
+  });
+});
+
+routes.delete("/delete", authMiddleware, (req, res) => {
+  const { admin_id } = req.body;
+
+  AdminsModel.findByIdAndRemove(admin_id, (err, result) => {
+    if (err) {
+      return res.status(400).send({
+        status: "Failed",
+        error: "Something went wrong!",
+      });
+    }
+  });
+
+  res.send({
+    status: "Success",
+  });
+});
+
+routes.patch("/update", authMiddleware, async (req, res) => {
+  const { name, lastName, phone, email, password, id } = req.body;
+
+  const updated = await AdminsModel.findByIdAndUpdate(id, {
+    name,
+    lastName,
+    phone,
+    email,
+  });
+
+  if (!updated) {
+    return res.status(400).send({
+      status: "Failed",
+      error: "Something went wrong!",
+    });
+  }
+
+  if (password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    AdminsModel.findByIdAndUpdate(
+      id,
+      { password: hashedPassword },
+      (err, result) => {
+        if (err) {
+          return res.status(400).send({
+            status: "Failed",
+            error: "Something went wrong!",
+          });
+        }
+      }
+    );
+  }
+
+  res.send({
+    status: "Success",
+  });
+});
+
+routes.get("/get-all", authMiddleware, async (req, res) => {
+  const admins = await AdminsModel.find().select("-password");
+
+  if (!admins) {
+    return res.status(400).send({
+      status: "Failed",
+      error: "No admins found!",
+    });
+  }
+
+  res.send({
+    admins,
+  });
+});
+
+routes.get("/:id", authMiddleware, async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).send({
+      status: "Failed",
+      error: "Admin not found!",
+    });
+  }
+
+  const admin = await AdminsModel.findById(id).select("-password");
+
+  if (!admin) {
+    return res.status(400).send({
+      status: "Failed",
+      error: "Admin not found!",
+    });
+  }
+
+  res.send(admin);
+});
 
 // routes.patch('/change-site-info', authMiddleware, async (req, res) => {
 //     const info = await PublicModel.create(req.body)
 //     res.send(info)
 // })
 
-module.exports = routes
+module.exports = routes;
--- app/routes/images.js
+++ app/routes/images.js
@@ -1,129 +1,136 @@
-const routes = require("express").Router()
-const authMiddleware = require("../middlewares/authenticate")
-const ImagesModel = require("../models/images")
-const upload = require("../utils/multer")
-const fs = require("fs")
+const routes = require("express").Router();
+const authMiddleware = require("../middlewares/authenticate");
+const ImagesModel = require("../models/images");
+const upload = require("../utils/multer");
+const fs = require("fs");
 
 routes.get("/", authMiddleware, async (req, res) => {
-    let { page } = req.query
-    let { limit } = req.query
+  let { page } = req.query;
+  let { limit } = req.query;
 
-    if (!page) {
-        page = 1
+  if (!page) {
+    page = 1;
+  }
+
+  if (!limit) {
+    limit = 9;
+  }
+
+  try {
+    const startIndex = (Number(page) - 1) * Number(limit);
+
+    const total = await ImagesModel.countDocuments({});
+    const images = await ImagesModel.find()
+      .sort({ _id: -1 })
+      .limit(Number(limit))
+      .skip(startIndex);
+
+    if (!images) {
+      return res.status(200).send({
+        status: "Failed",
+        error: "No images found",
+      });
     }
 
-    if (!limit) {
-        limit = 9
-    }
-
-    try {
-        const startIndex = (Number(page) - 1) * Number(limit)
-
-        const total = await ImagesModel.countDocuments({})
-        const images = await ImagesModel.find()
-            .sort({ _id: -1 })
-            .limit(Number(limit))
-            .skip(startIndex)
-
-        if (!images) {
-            return res.status(200).send({
-                status: "Failed",
-                error: "No images found",
-            })
-        }
-
-        res.json({
-            images,
-            page: Number(page),
-            numberOfPages: Math.ceil(total / Number(limit)),
-        })
-    } catch (error) {
-        res.status(404).send({
-            status: "Failed",
-            error: "Something went wrong",
-        })
-    }
-})
+    res.json({
+      images,
+      page: Number(page),
+      numberOfPages: Math.ceil(total / Number(limit)),
+    });
+  } catch (error) {
+    res.status(404).send({
+      status: "Failed",
+      error: "Something went wrong",
+    });
+  }
+});
 
 routes.post(
-    "/post",
-    authMiddleware,
-    upload.array("images"),
-    async (req, res) => {
-        if (!req.files) {
+  "/post",
+  authMiddleware,
+  upload.array("images"),
+  async (req, res) => {
+    if (!req.files) {
+      return res.status(400).send({
+        status: "Failed",
+        error: "There is no file!",
+      });
+    }
+
+    if (req.files.length <= 0) {
+      return res.status(400).send({
+        status: "Failed",
+        error: "There is no file!",
+      });
+    }
+
+    req.files.forEach((img) => {
+      ImagesModel.create(
+        { url: `${process.env.BACKEND_URL}/${img.filename}` },
+        (err, result) => {
+          if (err) {
             return res.status(400).send({
-                status: "Failed",
-                error: "There is no file!",
-            })
+              status: "Failed",
+              error: "Something went wrong",
+            });
+          }
         }
-
-        req.files.forEach((img) => {
-            ImagesModel.create(
-                { url: `${process.env.BACKEND_URL}/${img.filename}` },
-                (err, result) => {
-                    if (err) {
-                        return res.status(400).send({
-                            status: "Failed",
-                            error: "Something went wrong",
-                        })
-                    }
-                }
-            )
-        })
-
-        try {
-            res.status(200).send({
-                status: "Success",
-                uploaded: true,
-                url: req.files,
-            })
-        } catch (error) {
-            res.status(400).send({
-                status: "Failed",
-                error,
-            })
-        }
-    }
-)
-
-routes.delete("/delete", authMiddleware, async (req, res) => {
-    const { image } = req.body
-
-    if (!image) {
-        return res.status(400).send({
-            status: "Failed",
-            error: "Something went wrong!",
-        })
-    }
-
-    ImagesModel.findOneAndDelete({ url: image }, (err, result) => {
-        if (err) {
-            return res.status(400).send({
-                status: "Failed",
-                error: "Something went wrong!",
-            })
-        }
-    })
-
-    const backendLength = process.env.BACKEND_URL.length + 1
-    const urlLength = image.length - backendLength
-    const file_name = image.substr(backendLength, urlLength)
-    const path = `./public/${file_name}`
-
-    if (fs.existsSync(path)) {
-        fs.unlink(path, (err) => {})
-    }
+      );
+    });
 
     try {
-        return res.status(200).send({
-            status: "Success",
-        })
+      res.status(200).send({
+        status: "Success",
+        uploaded: true,
+        url: req.files,
+      });
     } catch (error) {
-        return res.status(200).send({
-            status: "Failed",
-            error,
-        })
+      res.status(400).send({
+        status: "Failed",
+        error,
+      });
     }
-})
+  }
+);
 
-module.exports = routes
+routes.delete("/delete", authMiddleware, async (req, res) => {
+  const { image } = req.body;
+
+  if (!image) {
+    return res.status(400).send({
+      status: "Failed",
+      error: "Something went wrong!",
+    });
+  }
+
+  ImagesModel.findOneAndDelete({ url: image }, (err, result) => {
+    if (err) {
+      return res.status(400).send({
+        status: "Failed",
+        error: "Something went wrong!",
+      });
+    }
+  });
+
+  const backendLength = process.env.BACKEND_URL.length + 1;
+  const urlLength = image.length - backendLength;
+  const file_name = image.substr(backendLength, urlLength);
+  const path = `./public/${file_name}`;
+
+  if (fs.existsSync(path)) {
+    fs.unlink(path, (err) => {});
+  }
+
+  try {
+    return res.status(200).send({
+      status: "Success",
+    });
+  } catch (error) {
+    return res.status(200).send({
+      status: "Failed",
+      error,
+    });
+  }
+});
+
+module.exports = routes;
