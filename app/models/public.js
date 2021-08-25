2b0100b7655c7086ed1e579e2ffe9d9b61713e9d
ata parvin ghods <ataparvinghods@gmail.com>
2021-08-25 21:40:16 +0430

    Added about model

--- app/models/public.js
+++ app/models/public.js
@@ -1,21 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const PublicModel = mongoose.Schema({
-    phone: Number,
-    phone2: Number,
-    telephone: Number,
-    address: String,
-    name: String,
-    lastName: String,
-    email: String,
-    title: String,
-    brand: String,
-    brandPer: String,
-    socials: Array,
-    logo: String,
-    hero_bg: String,
-    nav_items: Array,
-    typings: Array,
-})
+  phone: Number,
+  phone2: Number,
+  telephone: Number,
+  address: String,
+  name: String,
+  lastName: String,
+  email: String,
+  title: String,
+  brand: String,
+  brandPer: String,
+  socials: Array,
+  logo: String,
+  hero_bg: String,
+  nav_items: Array,
+  typings: Array,
+  about: String,
+});
 
-module.exports = mongoose.model('public', PublicModel)
+module.exports = mongoose.model("public", PublicModel);
